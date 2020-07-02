a7b3057eef815c3ee55a26a384c603a55b5027d7
Corey Orndoff <Corey.orndoff@hotmail.com>
2020-07-02 10:00:16 -0400

    Added a highscore text file and function that checks players score to see if they made the high score list. If they did they will be told so and the high score list will print out

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Security.Permissions;
 
 namespace Over_Under
 {
@@ -6,6 +10,7 @@ namespace Over_Under
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello. Please enter your name: ");
             string name = Console.ReadLine();
             Console.WriteLine($"Hello {name}. Let's play Over Under");
@@ -62,8 +67,61 @@ namespace Over_Under
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine($"Wrong. The next number was {next}. Your score was {count}.");
+            checkingScore(count);
             Console.ResetColor();
             Console.WriteLine("\n\n Would you like to play again? [Y or N]");
+        }
+
+        /*static void HighScoreFile()                                   Commenting this out because I need to figure out how to gain permission to create a file
+        {                                                               As of now you must create the text file and input a 0 (zero) for the first three lines
+            string highScoreFile = @"C:\Users\corey\HighScore.txt";
+            
+            if(File.Exists(highScoreFile) == false)
+            {
+                using (FileStream fs = File.Create(highScoreFile))
+                {
+
+                    File.WriteAllText(highScoreFile, "0\n0\n0");
+                }
+            }
+        }*/
+
+        static void checkingScore(int count)
+        {
+            string highScoreFile = @"C:\Users\corey\HighScore.txt";
+
+            int first = int.Parse(File.ReadLines(highScoreFile).First());
+            int second = int.Parse(File.ReadLines(highScoreFile).Skip(1).First());
+            int third = int.Parse(File.ReadLines(highScoreFile).Skip(2).First());
+
+            if (count > first)
+            {
+                third = second;
+                second = first;
+                first = count;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Congrats you made the high score list!!! \nHigh Scores:\n{first}\n{second}\n{third}");
+                Console.ResetColor();
+            }
+            else if (count > second)
+            {
+                third = second;
+                second = count;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Congrats you made the high score list!!! \nHigh Scores:\n{first}\n{second}\n{third}");
+                Console.ResetColor();
+            }
+            else if (count > third)
+            {
+                third = count;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"Congrats you made the high score list!!! \nHigh Scores:\n{first}\n{second}\n{third}");
+                Console.ResetColor();
+            }
+            File.WriteAllText(highScoreFile, String.Empty);
+            TextWriter tw = new StreamWriter(highScoreFile, true);
+            tw.WriteLine($"{first}\n{second}\n{third}");
+            tw.Close();
         }
     }
 }
